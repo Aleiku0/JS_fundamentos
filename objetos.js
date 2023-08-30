bee90fc79d1f0d0d9203ed092c07a6939a318a4c
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-08-30 19:29:30 -0300

    Update objetos.js

---
+++ objetos.js
@@ -0,0 +1,15 @@
+//objetos
+const mascota = {
+    nombre: 'Tom',
+    edad: 10,
+    vivo: true,
+    juguetes: ['ratón','catnip','pelota']
+}
+
+console.log (mascota);
+
+mascota.id = 1;
+
+console.log (mascota.id);
+
+console.log (mascota.juguetes[1]);
